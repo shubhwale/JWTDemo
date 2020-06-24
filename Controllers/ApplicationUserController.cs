6c471ffd5c53f235f25fa7d8e3bd2face584e1b8
Shubham Kawale <shubhamkawale@gmail.com>
2020-06-24 12:58:05 +0530

    First commit

---
+++ Controllers/ApplicationUserController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signinManager;
+        private readonly ApplicationSettings _appSettings;
+        public ApplicationUserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signinManager, IOptions<ApplicationSettings> appSettings)
+        {
+            _userManager = userManager;
+            _signinManager = signinManager;
+            _appSettings = appSettings.Value;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        //POST api/applicationuser/register
+        public async Task<Object> PostApplicationUser(ApplicationUserModel model) {
+            var applicationUser = new ApplicationUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+                PasswordHash = model.Password,
+                FullName = model.FullName
+            };
+            try
+            {
+                var result = await _userManager.CreateAsync(applicationUser, model.Password);
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        //POST api/applicationuser/login
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            var key = Encoding.UTF8.GetBytes(_appSettings.JWT_Secret);  
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if(user!=null && await _userManager.CheckPasswordAsync(user,model.Password))
+            {
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim("UserId",user.Id.ToString())
+                    }),
+                    Expires = DateTime.UtcNow.AddMinutes(5),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                };
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+                var token = tokenHandler.WriteToken(securityToken);
+                return Ok(new { token });
+            }
+            else
+            {
+                return BadRequest( new { message = "Username or Password is wrong"});
+            }
+        }
+    }
+}
